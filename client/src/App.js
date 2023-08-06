426d9b26fa1e553d99e88a6f933084af2814c9f2
simply_ark0 <arkojeetbera@simply-arko.local>
2023-08-06 13:49:21 +0530

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Category from "./components/Category/Category";
+import SingleProduct from "./components/SingleProduct/SingleProduct";
+import Newsletter from "./components/Footer/Newsletter/Newsletter";
+import AppContext from "./utils/context";
+
+function App() {
+    return (
+        <div>
+            <BrowserRouter>
+                <AppContext>
+                    <Header />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/category/:id" element={<Category />} />
+                        <Route
+                            path="/product/:id"
+                            element={<SingleProduct />}
+                        />
+                    </Routes>
+                    <Newsletter />
+                    <Footer />
+                </AppContext>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
